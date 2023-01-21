e8c9cf9293a6ed45de726c5a3b76bbab57f109e0
Vedamir <grandros1@gmail.com>
2023-01-21 13:00:21 +0800

    Task 10

---
+++ task10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int x = n/10;
+
+if(n < 100 || n >= 1000)
+Console.WriteLine("число должно  быть трехзначным");
+else
+Console.WriteLine(x % 10);
